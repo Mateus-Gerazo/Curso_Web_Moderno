d3f39430d45f3b439ad3babac456592709b96f20
Mateus_Gerazo <mateusgerazo@outlook.com>
2026-09-19 01:00:52 -0300

    Adicionando aulas sobre funções (callback, closure, contexto léxico, factory, construtora e declarações)

---
+++ Funcao/funcaoConstrutura.js
@@ -0,0 +1,31 @@
+function Carro(velociadeMaxima = 200, delta = 5) {
+    //Atributo privado
+    let velociadeAtual = 0
+
+    // Método público
+    this.acelerar = function () {
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+    // Método público
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+
+console.log(typeof Carro)
+console.log(typeof ferrari)
