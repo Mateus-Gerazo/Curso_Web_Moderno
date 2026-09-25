486750aa176132e56fc6c8730779f8d7d39eea18
Mateus_Gerazo <mateusgerazo@outlook.com>
2026-09-25 15:00:10 -0300

    Adiciona exercícios de função e anotações iniciais sobre objetos

---
+++ Objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        return 'processamento...'
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Princípios importantes
+// 1. Abstração: Representar uma entidade do mundo real em código.
+// 2. Encapsulamento: Ocultar detalhes de implementação.
+// 3. Herança (Prototype): Reutilização de código.
+// 4. Polimorfismo: Capacidade de um objeto assumir diferentes formas.
